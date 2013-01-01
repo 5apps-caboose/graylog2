a137665860215ac595ede47820fd9c25ad5f64d8
Sebastian Kippe <sebastian@kip.pe>
2013-01-01 21:22:23 +0700

    Upgrade to latest Graylog2 release candidate
    - Use new (and more complete) Graylog config file
    - Add Elasticsearch config file for embedded one
    - Add some more default attributes and use values from release example file

--- graylog2/attributes/default.rb
+++ graylog2/attributes/default.rb
@@ -1,18 +1,30 @@
 default.graylog2.basedir = "/var/graylog2"
-default.graylog2.server.version = "0.9.6p1"
-default.graylog2.web_interface.version = "0.9.6p1"
+default.graylog2.server.version = "0.10.0-rc.1"
+default.graylog2.web_interface.version = "0.10.0-rc.1"
 
 default.graylog2.elasticsearch.version="0.20.2"
 
+default.graylog2.port = 514
+
 default.graylog2.mongodb.host = "localhost"
 default.graylog2.mongodb.port = 27017
-default.graylog2.mongodb.max_connections = 150
+default.graylog2.mongodb.max_connections = 100
 default.graylog2.mongodb.database = "graylog2"
 default.graylog2.mongodb.auth = "false"
 default.graylog2.mongodb.user = "user"
 default.graylog2.mongodb.password = "password"
 
-default.graylog2.port = 514
+default.graylog2.transport_email.enabled = false
+default.graylog2.transport_email.hostname = "mail.example.com"
+default.graylog2.transport_email.port = 587
+default.graylog2.transport_email.use_auth = true
+default.graylog2.transport_email.use_tls = true
+default.graylog2.transport_email.auth_username = "you@example.com"
+default.graylog2.transport_email.auth_password = "secret"
+default.graylog2.transport_email.subject_prefix = "[graylog2]"
+default.graylog2.transport_email.from_email = "graylog2@example.com"
+default.graylog2.transport_email.from_name = "Graylog2"
+
 default.graylog2.collection_size = 50000000
 default.graylog2.email = "graylog2@example.org"
 default.graylog2.email_package = "postfix"
--- graylog2/recipes/server.rb
+++ graylog2/recipes/server.rb
@@ -69,6 +69,11 @@ template "/etc/graylog2.conf" do
   mode 0644
 end
 
+# Create graylog2-elasticsearch.yml
+template "/etc/graylog2-elasticsearch.yml" do
+  mode 0644
+end
+
 # Create init.d script
 template "/etc/init.d/graylog2" do
   source "graylog2.init.erb"
